73e37737c1d956f69262883663f419aff9820ba6
merlinletekos <beauvais.alan@gmail.com>
2023-07-24 23:03:52 +0200

    Connect EliteDangerous to the soft

--- EliteDangerousControllers/Program.cs
+++ EliteDangerousControllers/Program.cs
@@ -1,11 +1,22 @@
 ﻿
+using EliteAPI.Events.Status.Ship.Events;
+using EliteDangerousControllers.src;
+
 namespace EliteDangerousControllers
 {
     class EliteDangerousControllers
     {
         public static void Main()
         {
-            Console.WriteLine("Hello Elite!");
+            EliteDangerousControllers eliteDangerousControllers = new EliteDangerousControllers();
+            eliteDangerousControllers.Running();
+        }
+
+        private void Running()
+        {
+            EliteConnection eliteConnection = new EliteConnection();
+            eliteConnection.StartApi();
+            eliteConnection.ListenEvent();
         }
     }
 }
